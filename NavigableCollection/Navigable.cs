c92c9028b92c44568f90c76746a12616ef1c5e17
Marcos Cordeiro <mvmcjunior@gmail.com>
2019-12-13 23:44:25 -0300

    Create Navigable.cs

---
+++ NavigableCollection/Navigable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace NavigableCollection
+{
+    public class Navigable<T> : List<NavigableEntry<T>>
+    {
+        public Navigable(IEnumerable<T> items)
+        {
+            AddRange(BuildCollection(items));
+        }
+
+        private static IEnumerable<NavigableEntry<T>> BuildCollection(IEnumerable<T> items)
+        {
+            T pastItem = default;
+            NavigableEntry<T> pastEntry = default;
+            
+            foreach (var currentItem in items)
+            {
+                var entry = new NavigableEntry<T>
+                {
+                    Previous = pastItem,
+                    Current = currentItem
+                };
+                
+                if (pastEntry != null)
+                {
+                    pastEntry.Next = entry.Current;
+                }
+
+                yield return entry;
+
+                pastItem = currentItem;
+                pastEntry = entry;
+            }
+        }
+    }
+}
