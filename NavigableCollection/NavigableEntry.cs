a4cda9241945de815667347f3515444e4177fd41
Marcos Cordeiro <mvmcjunior@gmail.com>
2019-12-13 23:44:27 -0300

    Update NavigableEntry.cs

--- NavigableCollection/NavigableEntry.cs
+++ NavigableCollection/NavigableEntry.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-
-namespace NavigableCollection
+﻿namespace NavigableCollection
 {
     public class NavigableEntry<T>
     {
@@ -11,38 +7,5 @@ namespace NavigableCollection
         public T Current { get; set; }
         
         public T Next { get; set; }
-    }
-    
-    public class Navigable<T> : List<NavigableEntry<T>>
-    {
-        public Navigable(IEnumerable<T> items)
-        {
-            AddRange(BuildCollection(items));
-        }
-
-        private IEnumerable<NavigableEntry<T>> BuildCollection(IEnumerable<T> items)
-        {
-            T pastItem = default;
-            NavigableEntry<T> pastEntry = default;
-            
-            foreach (var currentItem in items)
-            {
-                var entry = new NavigableEntry<T>
-                {
-                    Previous = pastItem,
-                    Current = currentItem
-                };
-                
-                if (pastEntry != null)
-                {
-                    pastEntry.Next = entry.Current;
-                }
-
-                yield return entry;
-
-                pastItem = currentItem;
-                pastEntry = entry;
-            }
-        }
     }
 }
