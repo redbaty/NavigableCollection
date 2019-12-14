5832d704df7012557695e6335f1ee170873c8184
Marcos Cordeiro <mvmcjunior@gmail.com>
2019-12-13 23:44:30 -0300

    Create NavigableExtensions.cs

---
+++ NavigableCollection/NavigableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace NavigableCollection
+{
+    public static class NavigableExtensions
+    {
+        public static Navigable<T> ToNavigable<T>(this IEnumerable<T> baseCollection) =>
+            new Navigable<T>(baseCollection);
+    }
+}
