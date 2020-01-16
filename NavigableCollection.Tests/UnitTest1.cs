da4879c71e142291b4df690016203bad8ee0aeea
Marcos Cordeiro <marcos.cordeiro@cedrotech.com>
2020-01-16 18:36:48 -0300

    Update UnitTest1.cs

--- NavigableCollection.Tests/UnitTest1.cs
+++ NavigableCollection.Tests/UnitTest1.cs
@@ -11,21 +11,29 @@ namespace NavigableCollection.Tests
         public void TestMethod1()
         {
             dotMemory.Check();
-            var stringItems = new[] {"hello", "world", "!"};
+            var stringItems = new[]
+            {
+                    "hello",
+                    "world",
+                    "!"
+            };
             var navigableCollection = new Navigable<string>(stringItems);
 
             dotMemory.Check(memory =>
             {
-                var objectSet = memory.GetObjects(where => @where.Type.Is<Navigable<string>>());
-                Console.WriteLine(objectSet.SizeInBytes);
+                var objectSet = memory.GetObjects(where => @where.Type.Is<NavigableEntry<string>>());
+
+                Console.WriteLine("{0} objects found, with a total size of {1}",
+                        objectSet.ObjectsCount,
+                        objectSet.SizeInBytes);
             });
-            
+
             for (var index = 0; index < navigableCollection.Count; index++)
             {
                 var navigableEntry = navigableCollection[index];
 
-                Assert.AreEqual(navigableEntry.Current,stringItems[index]);
-                
+                Assert.AreEqual(navigableEntry.Current, stringItems[index]);
+
                 if (index < 2)
                 {
                     Assert.AreEqual(navigableEntry.Next, stringItems[index + 1]);
